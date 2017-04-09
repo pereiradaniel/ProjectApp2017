7984ac1052e7fe140ed8ca5ad967db157c8ea5b7
Daniel Pereira <pereiradpg@yahoo.ca>
2017-04-09 14:33:43 -0400

    Orders model test passes Should be invalid without a total Total is an integer used to store the total for the order

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170407183904) do
+ActiveRecord::Schema.define(version: 20170409181804) do
 
   create_table "orders", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "total"
   end
 
 end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,16 +1,22 @@
 require 'test_helper'
 # Tests
+# 	- Should be invalid without a total
+# 	- Total should be an integer
 # 	- Should be invalid without an item
 # 	- An order can have many items
-# 	- Total should be an integer
-# 	- Should be invalid without a total
 # 	- Should be invalid without a delivery address
 # 	- Should be invalid without contact information
 
 class OrderTest < ActiveSupport::TestCase
-	test "should be invalid without an item" do
-		orders.(:one).items = nil
-		assert_nil orders.(:one).items
+	# test "should be invalid without an item" do
+	# 	orders.(:one).items = nil
+	# 	assert_nil orders.(:one).items
+	# 	assert_equal false, orders(:one).valid?
+	# end
+
+	test "Should be invalid without a total" do
+		orders(:one).total = nil
+		assert_nil orders(:one).total
 		assert_equal false, orders(:one).valid?
 	end
 end
