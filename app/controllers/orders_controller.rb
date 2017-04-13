1f49897836168d944975716c07a019567f8bd61a
Daniel Pereira <pereiradpg@yahoo.ca>
2017-04-13 14:26:33 -0400

    Set up controllers and templates This is for next set of tests but shouldn't be doing much here Doing this is not exact TDD protocol

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,16 +3,15 @@ class OrdersController < ApplicationController
 		@order = Order.new
 	end
 	def create
-		# @order = Order.new(order_params)
-
-    # if @order.save
-    #   redirect_to items_url
-    # else
-    #   render items_url
-    # end
+		@order = Order.new
+    if @order.save
+      redirect_to order_path(@order.id)
+    else
+      redirect_to items_path
+    end
 	end
-  private
-  def order_params
-    params.require(:order).permit(:total, :items, :delivery_address, :contact_information)
-  end
+  # private
+  # def order_params
+  #   params.require(:order).permit()
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 	root "items#index"
 	resources :items, only: [:index]
-	resources :orders, only: [:new, :create]
+	resources :orders, only: [:new, :create, :show]
 end
