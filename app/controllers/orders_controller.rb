349d5a9ebccdc4a29904c102d2c1fadb9d8379a8
Daniel Pereira <pereiradpg@yahoo.ca>
2017-04-13 13:50:01 -0400

    Fleshed out orders#create routes Adjusting ideas for app flow and templating

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,6 +3,16 @@ class OrdersController < ApplicationController
 		@order = Order.new
 	end
 	def create
-		@order = Order.create
+		# @order = Order.new(order_params)
+
+    # if @order.save
+    #   redirect_to items_url
+    # else
+    #   render items_url
+    # end
 	end
+  private
+  def order_params
+    params.require(:order).permit(:total, :items, :delivery_address, :contact_information)
+  end
 end
