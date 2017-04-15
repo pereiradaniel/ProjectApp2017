c0b9ccbfa509755ea05328252fe686b2c7937e70
Daniel Pereira <pereiradpg@yahoo.ca>
2017-04-15 14:15:30 -0400

    Add form for new order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,6 +3,15 @@ class OrdersController < ApplicationController
 		@order = Order.new
 	end
 	def create
-		@order = Order.new
+		@order = Order.new(order_params)
+    if @order.save
+      redirect_to orders_path(@order.id)
+    else
+      redirect_to items_path
+    end
 	end
+  private
+  def order_params
+    params.require(:order).permit(:total, :contact_information, :delivery_address, :items)
+  end
 end
