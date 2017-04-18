9fda67679c5768bb5c6038e0dc1c6c1264c4ec45
Daniel <truenorthtrainingcompany@gmail.com>
2017-04-18 17:01:07 -0400

    Added new items column just plain text

---
+++ db/migrate/20170418210022_add_new_items_column_to_orders_table.rb
@@ -0,0 +1,5 @@
+class AddNewItemsColumnToOrdersTable < ActiveRecord::Migration[5.0]
+  def change
+    add_column :orders, :items, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418205603) do
+ActiveRecord::Schema.define(version: 20170418210022) do
 
   create_table "items", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20170418205603) do
     t.integer  "total"
     t.text     "delivery_address"
     t.text     "contact_information"
+    t.text     "items"
   end
 
 end
